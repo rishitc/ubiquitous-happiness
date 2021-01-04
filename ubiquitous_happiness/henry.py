106d779eb95eebfa1c08548a931148195e417115
Rishit Chaudhary <rishit.c@live.com>
2021-01-04 17:57:18 +0530

    Revamped the UI - Ver: 0.1

--- ubiquitous_happiness/henry.py
+++ ubiquitous_happiness/henry.py
@@ -6,6 +6,9 @@ are interested in converting into a lab report.
 """
 import tkinter as tk
 import tkinter.font as tkFont
+from tkinter import Label, ttk
+
+
 from tkinter import filedialog, messagebox
 from ubiquitous_happiness.logger import logging
 
@@ -41,71 +44,87 @@ def onExit():
 
 def file_explorer():
     notebook = logging.getLogger(__name__)
-    notebook.info("el")
+
     # Create the root window
     window = tk.Tk()
+    window.title('File Explorer')  # Set the window title
+    window.geometry("600x600")  # Set the window size
+    # Set the background color of the window
+    window.configure(background="#E9E3E6")
+    notebook.debug("Top-level window has been created.")  # Log this step
 
-    TITLE_STYLE = tkFont.Font(family="Lucida Grande", size=20)
-    SUB_TITLE_STYLE = tkFont.Font(family="Lucida Grande", size=16)
+    frame_titles = tk.Frame(window)
 
-    # Set the window title
-    window.title('File Explorer')
-
-    # Set the window size
-    window.geometry("500x500")
-
-    # Set window background color
-    window.config(background="white")
+    # Create the font styles of the title and subtitle
+    TITLE_STYLE = tkFont.Font(family="Lucida Grande", size=20, weight="bold")
+    SUBTITLE_STYLE = tkFont.Font(family="Lucida Grande", size=16,
+                                 slant="italic")
 
     # Create a File Explorer label
     label_frame_title = tk.Label(window,
                                  text="Ubiquitous Happiness",
-                                 width=100, height=2,
-                                 fg="blue",
-                                 bg="white",
+                                 height=1,
+                                 fg="#07020D",
+                                 bg="#E9E3E6",
                                  font=TITLE_STYLE,
-                                 padx=0,
+                                 padx=52,
                                  pady=0)
 
     # Create a File Explorer label
     label_frame_subtitle = tk.Label(window,
-                                    text=("That annoying report is just a few"
-                                          " clicks away"),
-                                    width=100, height=1,
-                                    fg="blue",
-                                    bg="white",
-                                    font=SUB_TITLE_STYLE,
-                                    padx=0,
+                                    text=("That annoying report is just a"
+                                          " few clicks away!"),
+                                    height=1,
+                                    fg="#07020D",
+                                    bg="#E9E3E6",
+                                    font=SUBTITLE_STYLE,
+                                    padx=52,
                                     pady=0)
 
-    label_file_explorer = tk.Label(window,
-                                   text="",
-                                   width=100, height=1,
-                                   fg="blue")
+    # Vertical line to separate the frame title and subtitle from the body
+    # of the frame
+    heading_sep = ttk.Separator(window, orient="horizontal")
 
-    button_explore = tk.Button(window,
-                               text="Browse Files",
-                               command=lambda:
-                               browseFiles(label_file_explorer))
+    # frame = tk.Frame(window)
+    # label_file_explorer = tk.Label(frame,
+    #                                text="The file",
+    #                                height=1,
+    #                                fg="blue")
+    # label_file_explorer.pack(side=tk.LEFT)
 
-    # Button to exit the program
-    button_exit = tk.Button(window,
-                            text="Exit",
-                            command=onExit)
+    # label_file_found = tk.Label(frame,
+    #                             text="The file",
+    #                             height=1,
+    #                             fg="blue")
+    # label_file_found.pack(side=tk.RIGHT)
+
+    # button_explore = tk.Button(window,
+    #                            text="Browse Files",
+    #                            command=lambda:
+    #                            browseFiles(label_file_explorer))
+
+    # # Button to exit the program
+    # button_exit = tk.Button(window,
+    #                         text="Exit",
+    #                         command=onExit)
+
+    label_frame_title.pack(side=tk.TOP, fill=tk.X)
+    label_frame_subtitle.pack(side=tk.TOP, fill=tk.X)
+    heading_sep.pack(side=tk.TOP, ipadx=600, fill=tk.X)
 
     # Grid method is chosen for placing
     # the widgets at respective positions
     # in a table like structure by
     # specifying rows and columns
-    label_frame_title.grid(column=0, row=0)
-    label_frame_subtitle.grid(column=0, row=1)
-    label_file_explorer.grid(column=0, row=2)
+    # label_frame_title.grid(column=0, row=0)
+    # label_frame_subtitle.grid(column=0, row=1)
+    # s.grid(column=0, row=2, sticky='ew')
+    # label_file_explorer.grid(column=0, row=3)
 
-    button_explore.grid(column=0, row=3)
+    # button_explore.grid(column=0, row=4)
+    # button_exit.grid(column=0, row=5)
 
-    button_exit.grid(column=0, row=4)
-
-    # Let the window wait for any events
+    # Start the event loop and let the window wait for any events
     window.mainloop()
 
 
