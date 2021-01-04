27bc8310f908302fa8740df80b78ad2f0ae1b01a
Rishit Chaudhary <rishit.c@live.com>
2021-01-05 03:48:40 +0530

    Fixed formatting, UI and minor bug fixes

--- ubiquitous_happiness/clifford.py
+++ ubiquitous_happiness/clifford.py
@@ -6,12 +6,15 @@ are interested in converting into a lab report.
 """
 import os
 import sys
+from rich.text import Text
 
 
 from ubiquitous_happiness.console import console
-from ubiquitous_happiness.henry import FOLDER_PATH, file_explorer
+import ubiquitous_happiness.henry as navigator
 from ubiquitous_happiness.logger import logging
 from ubiquitous_happiness.folder_protocol import FolderIntegrity
+
+SUPPORTED_IMAGE_TYPES = ["jpg", "jpeg", "png"]
 
 if __name__ == "__main__":
     notebook = logging.getLogger(__name__)  # Create a logger for this file
@@ -30,31 +33,34 @@ if __name__ == "__main__":
     console.print(("[bold cyan]So here are the steps you need to"
                   " follow to generate your report:"), overflow="ellipsis")
     console.print("1. Select the target folder.", overflow="ellipsis")
-    console.print("2. Configure the selected files.", overflow="ellipsis")
-    console.print("3. ", overflow="ellipsis")
-    console.print("4. ", overflow="ellipsis")
-    console.print("5. ", overflow="ellipsis")
-
+    console.print("2. Enter some metadata")
+    console.print("3. Enjoy the generated results.", overflow="ellipsis")
+    console.rule("")
     logging.info("Steps explained.")
 
     logging.info("File explorer launched.")
-    file_explorer()
+    FOLDER_PATH = navigator.file_explorer()
     logging.info("File explorer completed.")
 
-    logging.debug("The path selected by the user is: %s", FOLDER_PATH)
+    logging.debug("The path selected by the user is: %s",
+                  FOLDER_PATH)
 
-    # chk: bool = False
-    # fi: FolderIntegrity = FolderIntegrity()
-    # if FOLDER_PATH is not None and FOLDER_PATH != "":
-    #     with os.scandir(FOLDER_PATH) as root:
-    #         for entry in root:
-    #             if not entry.name.startswith('.'):
-    #                 fi.chk_folder_base(entry)
-    #     folder_contents = os.listdir(path=FOLDER_PATH)
-    #     chk = fi.compile_result()
+    if FOLDER_PATH is None:
+        console.print("[bold red]ERROR:[/bold red] Folder Path is None.")
+        raise FileNotFoundError("Folder path selected is None!")
 
-    #     if chk is False:  # If the folder is not the right format
-    #         console.print("[bold red]ERROR:[/bold red] The folder format is not right!")
-    #         logging.error("")
-    #         sys.exit(1)
-
+    for root, dirs, files in os.walk(FOLDER_PATH):
+        for file in files:
+            if os.path.splitext(file) not in SUPPORTED_IMAGE_TYPES:
+                # Get the complete path to the error-causing file
+                err_file = os.path.join(root, file)
+                print()  # Simple blank line for formatting
+                console.print(("[bold red]ERROR:[/bold red] The file "
+                              f"\"{err_file}\" is [bold]not[/bold] of a "
+                               "supported file type."), overflow="ellipsis")
+                console.print(("[bold cyan]INFO:[/bold cyan] The supported "
+                              f"file types are: {SUPPORTED_IMAGE_TYPES}"),
+                              overflow="ellipsis")
+                print()  # Simple blank line for formatting
+                console.rule(Text("Goodbye!", style="bold #B80C09"),
+                             style="#B80C09")
