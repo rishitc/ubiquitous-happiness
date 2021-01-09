40f0c5ac22fab9d72b7909a726ecc127c5fb6150
Rishit Chaudhary <rishit.c@live.com>
2021-01-10 00:47:48 +0530

    Added class for exit codes

--- ubiquitous_happiness/config.py
+++ ubiquitous_happiness/config.py
@@ -1,3 +1,10 @@
-"""Module containing application level settings."""
+"""Module containing application level settings and exit codes."""
 
 DEBUG_MODE = True
+
+
+class ExitCodes():
+    """Exit codes for various condition."""
+
+    FILE_NOT_FOUND = 1
+    UNSUPPORTED_IMAGES = 2
